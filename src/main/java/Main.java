f458931624496b8e429c63b183ae16e92417ae55
Thanhnguyen11111 <wtfking54@gmail.com>
2025-01-16 16:20:19 +0700

    Bai01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 42;
+        double b = 3.14;
+        String c = "Hello, Java!";
+        System.out.println(a);
+        System.out.println(b); 
+        System.out.println(c);
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
         // Ví dụ:
